936f1bfe5b02546d465e830e4966db3bf320e25e
affe334422 <alle07002@utb.vaxjo.se>
2024-09-02 10:28:36 +0200

    Gissa-numret

---
+++ Gissa.cs
@@ -0,0 +1,31 @@
+
+
+
+Random rnd =new Random();
+
+int x = rnd.Next(1, 101);
+
+Console.WriteLine("Du ska gissa nummret x som är mellan 0-100");
+int a;
+do{
+Console.Write("Gissa: ");
+string str = Console.ReadLine();
+a = int.Parse(str);
+
+if(a>x){
+Console.Write("x är mindre");
+Console.WriteLine(" ");
+    if(a<=x+10){
+        console
+    }
+}
+else if(a<x){
+    Console.Write("x är större");
+    Console.WriteLine(" ");
+}
+
+}
+while(a != x);
+
+
+Console.Write("korrekt");
